bf933e86056b34c67b15bea410f60b0bc57455d5
iPeakajoo <peakajoo@gmail.com>
2023-10-25 16:17:50 +0700

    update

--- js/article.js
+++ js/article.js
@@ -12,11 +12,16 @@ async function getblog3() {
 
   json.blogs.forEach(item => {
     if (item.id == id) {
-      output3 += `<li id='${item.id}'>${item.title}
-              </br> ${item.author}
-              </br> ${item.category}
-              </br> ${item.content}
-              </br> ${item.views}</li>`
+      output3 += `<div class="card col-lg-3 col-md-4 col-sm-1"  style="width: 100%;">
+      <img src="${item.img}" class="card-img-top" alt="...">
+      <div class="card-body">
+      <h5 class="card-title">${item.title}</h5>
+      <p class="card-text">${item.content}</p>
+      <p class="card-text">view ${item.views}</p>
+      
+      </div>
+      </div>
+  `
     }
   });
   article.innerHTML = output3
