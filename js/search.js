cf94a732e2469367a76d1909bd23a2374da63956
iPeakajoo <peakajoo@gmail.com>
2023-10-25 16:11:20 +0700

    up

--- js/search.js
+++ js/search.js
@@ -10,12 +10,16 @@ async function getblog4() {
   const json = await res.json();
 
   json.blogs.forEach((item) => {
-    output3 += `<li id='${item.id}'>${item.title}
-                  </br> ${item.author}
-                  </br> ${item.category}
-                  </br> ${item.content}
-                  </br> <a href="article.html?id=${item.id}">Read more</a>
-                  </br> ${item.views}</li>`;
+    output3 += `<div class="card col-lg-3 col-md-4 col-sm-1"  style="width: 18rem;">
+    <img src="${item.img}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${item.title}</h5>
+    <p class="card-text">${item.content}</p>
+    <p class="card-text">view ${item.views}</p>
+    <a href="article.html?id=${item.id}" class="btn btn-primary">Read more</a>
+    </div>
+    </div>
+`;
   });
   matchList.innerHTML = output3;
 }
@@ -41,12 +45,16 @@ SearchBtn.addEventListener("click", () => {
     // แสดงผลการค้นหาคำที่ได้
 
     match.forEach((item) => {
-      output4 += `<li id='${item.id}'>${item.title}
-              </br> ${item.author}
-              </br> ${item.category}
-              </br> ${item.content}
-              </br> <a href="article.html?id=${item.id}">Read more</a>
-              </br> ${item.views}</li>`;
+      output4 += `<div class="card col-lg-3 col-md-4 col-sm-1"  style="width: 18rem;">
+      <img src="${item.img}" class="card-img-top" alt="...">
+      <div class="card-body">
+      <h5 class="card-title">${item.title}</h5>
+      <p class="card-text">${item.content}</p>
+      <p class="card-text">view ${item.views}</p>
+      <a href="article.html?id=${item.id}" class="btn btn-primary">Read more</a>
+      </div>
+      </div>
+  `;
     });
 
     matchList.innerHTML = output4;
